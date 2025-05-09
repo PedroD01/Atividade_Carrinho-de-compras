ba9231af7b5c4c1cbef7c92e818ff4d0180e13e0
Pedro Duarte <pdjtresr431@gmail.com>
2025-05-09 13:15:55 -0300

    adicionado total dos produtos

--- js/app.js
+++ js/app.js
@@ -6,18 +6,13 @@ function adicionar() {
     let carrinho = document.getElementById('lista-produtos');
     let preco_carrinho = document.getElementById('valor-total');
     
-    let preço_produtos = [];
     let preco_total_produto = parseInt(preco) * parseInt(quantidade.value);
-    preço_produtos.push(preco_total_produto);
 
     let initialValue = 0;
     let soma = preço_produtos.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue); 
 
     carrinho.innerText += `${quantidade.value}x ${nome_produto} R$${preco}\n`;
     
-    
-    //console.log(preco_carrinho);
-    console.log(preço_produtos);
     console.log(soma);
 }
 function limpar() {
