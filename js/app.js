2c4aa4f839b931fadadf061b7af5bf20b8c19f3b
Pedro Duarte <pdjtresr431@gmail.com>
2025-05-09 13:55:21 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -8,15 +8,18 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nome_produto = produto.split('-')[0];
     let preco = produto.split('- R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    if(quantidade <= 0){
+        alert("ERRO! Favor digitar um número acima de 0 no campo Qtde.");
+        return;
+    }
     
-    
-    let preco_total_produto = parseInt(preco) * parseInt(quantidade.value);
+    let preco_total_produto = parseInt(preco) * parseInt(quantidade);
 
     soma += preco_total_produto;
 
     carrinho.innerHTML += `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade.value}x</span> ${nome_produto} <span class="texto-azul">R$${preco}</span>
+          <span class="texto-azul">${quantidade}x</span> ${nome_produto} <span class="texto-azul">R$${preco}</span>
         </section>\n`;
     preco_carrinho.textContent = `R$${soma}`;
 
